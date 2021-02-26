fb94a16ed98cb69ddb89433a6f4b2051fcf7d92a
Piotr Acalski <piotr.acalski@cognifide.com>
2021-02-26 16:13:50 +0100

    dotenv init, minor code refactor

--- main.py
+++ main.py
@@ -1,41 +1,51 @@
 from google.cloud import bigquery
 from google.oauth2 import service_account
+from dotenv import load_dotenv
+import os
 
-key_path = "./credentials.json"
+load_dotenv()
 
-credentials = service_account.Credentials.from_service_account_file(
-    key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],
+GSA_CREDENTIALS_PATH = os.getenv('GSA_CREDENTIALS_PATH', './credentials.json')
+
+GSA_CREDENTIALS = service_account.Credentials.from_service_account_file(
+    GSA_CREDENTIALS_PATH,
+    scopes=["https://www.googleapis.com/auth/cloud-platform"]
 )
 
-# print(credentials)
+ORIGIN_URL = '%ford.co.uk'
 
-def query_device_distribution():
-  client = bigquery.Client(credentials=credentials, project=credentials.project_id)
-  query_job = client.query(
-    """
-    SELECT
-    form_factor.name AS Device,
-    COUNT(form_factor.name) AS CountOf,
-    ROUND(COUNT(form_factor.name) / (SELECT
-        COUNT(origin)
-        FROM `chrome-ux-report.all.202101`
-        WHERE origin LIKE '%ford.co.uk'), 2) * 100 AS Percentage
+class WebsiteHealthcheckWorker:
+  def __init__(self):
+    self.harvest = []
+    
+  def query_device_distribution(self):
+    client = bigquery.Client(credentials=GSA_CREDENTIALS, project=GSA_CREDENTIALS.project_id)
+    query_job = client.query(
+      f"""
+      SELECT
+      form_factor.name AS Device,
+      COUNT(form_factor.name) AS CountOf,
+      ROUND(COUNT(form_factor.name) / (SELECT
+          COUNT(origin)
+          FROM `chrome-ux-report.all.202101`
+          WHERE origin LIKE '{ORIGIN_URL}'), 2) * 100 AS Percentage
 
-    FROM 
-    `chrome-ux-report.all.202101`
+      FROM 
+      `chrome-ux-report.all.202101`
 
-    WHERE origin LIKE '%ford.co.uk'
+      WHERE origin LIKE '{ORIGIN_URL}'
 
-    GROUP BY form_factor.name
-    """
-  )
+      GROUP BY form_factor.name
+      """
+    )
 
-  results = query_job.result()
+    results = query_job.result()
 
 
-  for row in results:
-    print(f'Device: {row[0]} | Count: {row[1]} | Percentage: {row[2]}')
+    for row in results:
+      print(f'Device: {row[0]} | Count: {row[1]} | Percentage: {row[2]}')
 
 
 if __name__ == "__main__":
-    query_device_distribution()
+  worker = WebsiteHealthcheckWorker()
+  worker.query_device_distribution()
